5df57b84a4801995c83573eef574c4560b1d012d
Gurpreet Kaur <gurpreet.dhillon03@gmail.com>
2024-02-10 14:08:55 -0800

    Initial Commit

---
+++ AudioPlayer_SwiftUI/AudioPlayer_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayer_SwiftUIApp.swift
+//  AudioPlayer_SwiftUI
+//
+//  Created by ReetDhillon on 2024-02-10.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayer_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioPlayer_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioPlayer_SwiftUI
+//
+//  Created by ReetDhillon on 2024-02-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
